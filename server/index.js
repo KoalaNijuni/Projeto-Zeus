8b0afff9cfff995c3ba5e88d74f2052d8037c3a0
Victor Borborema <victorborborema@unifor.br>
2021-11-10 17:33:24 -0300

    Salvo pra continuar em casa

--- server/index.js
+++ server/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+//const router = require('./routes');
+
 const app = express();
+
+app.use(express.json());
+
+mongoose.connect(`mongodb+srv://${'koala22'}:${'koala123'}@cluster0.x7m3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected Succesfully");
+});
 
 app.get('/', (req, res) => {
     res.send('hello');
