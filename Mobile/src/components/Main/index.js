9239e0f6a1c64d8e2800614c6ece53e3eee4fc81
Koala <gabithekoala22@gmail.com>
2021-11-18 17:52:26 -0300

    Mobile

---
+++ Mobile/src/components/Main/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "../Form/index";
+import { View, Text } from "react-native";
+
+export default function Main() {
+  return (
+    <View>
+      <Form />
+    </View>
+  );
+}
